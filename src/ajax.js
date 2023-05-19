f0eb21ea52fa9671b3c564e30ce9d07ea17b8c50
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-05-19 09:43:28 +0200

    base

--- src/ajax.js
+++ src/ajax.js
@@ -1,7 +1,23 @@
 'use strict';
 
 $.namespace('pl.training.ajax', (function () {
+    const onReadySateChange = (xhr,  onSuccess, onFailure, format) => {
+        if (xhr.readyState === 4) {
+            if (xhr.status > 199 && xhr.status < 300) {
+                const result = format === 'json' ? JSON.parse(xhr.responseText) : xhr.responseText;
+                onSuccess(result);
+            } else {
+                onFailure(xhr.statusCode, xhr.status.status, xhr);
+            }
+        }
+    };
+    const request = ({method = 'GET', url, onSuccess, onFailure, format = 'json'}, data) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.onreadystatechange = () => onReadySateChange(xhr, onSuccess, onFailure, format);
+      xhr.send(data);
+    };
     return {
-      load: () => console.log('Not yet implemented')
+      request
     };
 })());
--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@
 
 $.onReady(() => {
     const ajax = $.namespace('pl.training.ajax');
-    ajax.load();
-    console.log(ajax);
-    $.on({selector: '#box', listener: function() { console.log(this) }});
+    //$.on({selector: '#box', listener: function() { console.log(this) }});
+    const onFailure = (code, description, xhr) => console.log(`Failure: ${code} ${description}`);
+    const baseUrl = 'http://localhost:3000';
+
+    /*ajax.request({
+        url: `${baseUrl}/users/1`,
+        onSuccess: (user) => {
+            const orderId = user.orders.pop().id;
+            ajax.request({
+                url: `${baseUrl}/orders/${orderId}`,
+                onSuccess: (order) => console.log(order),
+                onFailure
+            });
+        },
+        onFailure
+    });*/
+
+    const getFirstOrder = (user) => {
+        const orderId = user.orders.pop().id;
+        ajax.request({
+            url: `${baseUrl}/orders/${orderId}`,
+            onSuccess: (order) => console.log(order),
+            onFailure
+        });
+    }
+
+    ajax.request({
+        url: `${baseUrl}/users/1`,
+        onSuccess: getFirstOrder,
+        onFailure
+    });
 });
