2b938dd65f2ec84c02227e57e80e88e38aee1bb5
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-05-19 12:37:09 +0200

    base

--- src/index.js
+++ src/index.js
@@ -22,14 +22,27 @@ $.onReady(() => {
         return subject;
     };
 
-    const values = observeInput('#value', 'keyup')
+    const subtractTaxes = (data) => data[1].reduce((result, tax) => result - result * (tax / 100), data[0]);
+
+    const value$ = observeInput('#value', 'keyup')
         .pipe(rxjs.debounceTime(1_000));
 
-    rxjs.combineLatest(observeCheckbox('#vat', 'click'), observeCheckbox('#cit', 'click'))
-      .subscribe({
-        next: (value) => console.log(value),
-        error: (error) => console.log(error)
-    });
+    const taxes$ = rxjs.combineLatest(observeCheckbox('#vat', 'click'), observeCheckbox('#cit', 'click'))
 
+    const subscription = rxjs.combineLatest(value$, taxes$)
+        .pipe(
+            rxjs.map(subtractTaxes)
+        )
+        .subscribe({
+            next: (value) => document.querySelector('#result').innerText = `Result: ${value}`,
+            error: (error) => console.log(error)
+        });
 
+    // subscription.unsubscribe();
+
+     rxjs.from(ajax.request({url: `http://localhost:3000/users/1`}))
+         .subscribe({
+             next: (value) => console.log(value),
+             error: (error) => console.log(error)
+         });
 });
